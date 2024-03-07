0b2972a84598352f7d854c836a3bd41c1ad16f56
victordelafu <160121458+victordelafu@users.noreply.github.com>
2024-03-07 11:13:29 +0100

    Nuevo Archivo.java
    Archivo java que complementa al otro archivo añadiendo la función de dividir los 2 números introducidos por teclado.

---
+++ GitHubEntornosDesarrollo/src/main/java/com/mycompany/githubentornosdesarrollo/NuevaFuncionalidad.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.githubentornosdesarrollo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class NuevaFuncionalidad {
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Archivo de prueba");
+        int num1 = entrada.nextInt();
+        int num2 = entrada.nextInt();
+        System.out.println("1->Suma\n2->Resta\n3->Multiplicación\n4->Salir");
+        int eleccion = entrada.nextInt();
+        do {
+            switch (eleccion) {
+                case 1:
+                    suma(num1,num2);
+                    break;
+                case 2:
+                    resta(num1,num2);
+                    break;
+                case 3:
+                    multiplicacion(num1,num2);
+                    break;
+                case 4: 
+                    division(num1,num2);
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa");
+                    return;
+                default:
+                    System.out.println("Elige una opción correcta");
+            }
+        } while (eleccion != 4);
+    }
+
+    private static void suma(int num1, int num2) {
+        System.out.println(num1+num2+" Es el resultado de la suma de los numeros");
+    }
+
+    private static void resta(int num1, int num2) {
+        System.out.println(num1-num2+" Es el resultado de la resta de los numeros");
+    }
+
+    private static void multiplicacion(int num1, int num2) {
+        System.out.println(num1*num2+" Es el resultado de la Multiplicación de los numeros");
+    }
+
+    private static void division(int num1, int num2) {
+        System.out.println(num1/num2+" Es el resultado de la División de los numeros");
+    }
+}
+
+    
+
